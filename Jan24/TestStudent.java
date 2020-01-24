e3230fcbf91ccab1ff841eaed834ee03f35ef6df
fullstack java <45912929+fullstackjavaprof@users.noreply.github.com>
2020-01-24 09:35:01 +0530

    Add files via upload

---
+++ Jan24/TestStudent.java
@@ -0,0 +1,13 @@
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+	     
+		Student s1=new Student();
+		Student s2=new Student();
+		s1.insertStudentData(101,"Kumar");
+        s1.display();
+        s2.insertStudentData(102, "Vasu");
+        s2.display();
+	}
+}
