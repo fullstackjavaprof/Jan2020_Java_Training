dbbc0098f24fb064cfd370fcfcd40a4b44ef9362
fullstack java <45912929+fullstackjavaprof@users.noreply.github.com>
2020-02-15 16:34:34 +0530

    Add files via upload

---
+++ Feb15/HelloWorldFEB2020/src/main/java/com/javafullstacktraining/HelloWorldFeb2020Application.java
@@ -0,0 +1,13 @@
+package com.javafullstacktraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldFeb2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldFeb2020Application.class, args);
+	}
+
+}
